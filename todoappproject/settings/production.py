586fdb2fec7447c193750c77e298b726da2a4067
Mohd Saquib <ans2sqb@gmail.com>
2020-06-07 10:54:49 +0530

    added postgres config for production

--- todoappproject/settings/production.py
+++ todoappproject/settings/production.py
@@ -1,9 +1,15 @@
 from .base import *
 import django_heroku
+import dj_database_url
 
 
 # DEBUG = config('DEBUG', cast=bool)
 DEBUG = True
+
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+print(DATABASES['default'])
 
 
 ALLOWED_HOSTS = ['zeesertodoapp.herokuapp.com']
