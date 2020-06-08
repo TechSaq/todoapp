ca698d56e21233c7998bea4b48672545bd13330c
Mohd Saquib <ans2sqb@gmail.com>
2020-06-08 10:59:16 +0530

    debug false

--- todoappproject/settings/production.py
+++ todoappproject/settings/production.py
@@ -3,19 +3,15 @@ import django_heroku
 import dj_database_url
 
 
-# DEBUG = config('DEBUG', cast=bool)
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
+# DEBUG = True
 
 DATABASE_URL = config('DATABASE_URL')
 
 db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-print(db_from_env)
-
 
 ALLOWED_HOSTS = ['zeesertodoapp.herokuapp.com']
-
-print(ALLOWED_HOSTS)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
