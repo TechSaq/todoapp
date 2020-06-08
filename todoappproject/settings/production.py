18dbed59a2c31b09313ebc3a515d4072137a1516
Mohd Saquib <ans2sqb@gmail.com>
2020-06-08 10:27:44 +0530

    set DEBUG False in production

--- todoappproject/settings/production.py
+++ todoappproject/settings/production.py
@@ -4,7 +4,7 @@ import dj_database_url
 
 
 # DEBUG = config('DEBUG', cast=bool)
-DEBUG = True
+DEBUG = False
 DATABASE_URL = config('DATABASE_URL')
 
 db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500)
